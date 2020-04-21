96347b15c896d52ba80db107e029d156c0af82df
Raymond Leung <leptanian@gmail.com>
2020-04-22 00:08:36 +1200

    Add complete chatbot with basic features

---
+++ client/src/components/chatbot/Card.jsx
@@ -0,0 +1,7 @@
+// import React from 'react'
+
+// const Card = (props) => {
+//   props.payload.fields.insertField.structValue.fields.
+// }
+
+// export default Card
--- client/src/components/chatbot/Chatbot.jsx
+++ client/src/components/chatbot/Chatbot.jsx
@@ -9,15 +9,19 @@ const cookies = new Cookies()
 
 export default class Chatbot extends Component {
 	messagesEnd
-	talkInput
+	chatInput
 
 	constructor(props) {
 		super(props)
 
+		this.hide = this.hide.bind(this)
+		this.show = this.show.bind(this)
 		this._handleInputKeyPress = this._handleInputKeyPress.bind(this)
+		// this._handleQuickReplyPayload = this._handleQuickReplyPayload.bind.(this)
 
 		this.state = {
-			messages: []
+			messages: [],
+			showBot: true
 		}
 
 		if (cookies.get('userID') === undefined) {
@@ -104,6 +108,25 @@ export default class Chatbot extends Component {
 		}
 	}
 
+	// _handleQuickReplyPayload(payload, text) {
+	// 	switch (payload) {
+	// 		case 'quick reply event':
+	// 			this.df_event_query('event prompt')
+	// 			break
+	// 		default:
+	// 			this.df_text_query(text)
+	// 			break
+	// 	}
+	// }
+
+	show() {
+		this.setState({ showBot: true })
+	}
+
+	hide() {
+		this.setState({ showBot: false })
+	}
+
 	_handleInputKeyPress(e) {
 		if (e.key === 'Enter') {
 			this.df_text_query(e.target.value)
@@ -112,25 +135,65 @@ export default class Chatbot extends Component {
 	}
 
 	render() {
-		return (
-			<>
-				<div style={{ height: 400, width: 400, float: 'right' }}>
+		if (this.state.showBot) {
+			return (
+				<>
 					<div
-						id="chatbot"
-						style={{ height: '100%', width: '100%', overflow: 'auto' }}
+						style={{
+							height: 400,
+							width: 400,
+							position: 'absolute',
+							bottom: 0,
+							right: 0
+						}}
 					>
-						<h2>Chatbot</h2>
-						{this.renderMessages(this.state.messages)}
 						<div
-							ref={(el) => {
-								this.messagesEnd = el
-							}}
-							style={{ float: 'left', clear: 'both' }}
-						></div>
-						<input type="text" onKeyPress={this._handleInputKeyPress} />
+							id="chatbot"
+							style={{ height: '100%', width: '100%', overflow: 'auto' }}
+						>
+							<ul id="nav-mobile" className="right hide-on-med-and-down">
+								<li>
+									<a onClick={this.hide}>Close</a>
+								</li>
+							</ul>
+							<h2>Chatbot</h2>
+							{this.renderMessages(this.state.messages)}
+							<div
+								ref={(el) => {
+									this.messagesEnd = el
+								}}
+								style={{ float: 'left', clear: 'both' }}
+							></div>
+							<input type="text" onKeyPress={this._handleInputKeyPress} />
+						</div>
 					</div>
+				</>
+			)
+		} else {
+			return (
+				<div
+					style={{
+						height: 40,
+						width: 400,
+						position: 'absolute',
+						bottom: 0,
+						right: 0
+					}}
+				>
+					<ul id="nav-mobile" className="right hide-on-med-and-down">
+						<li>
+							<a onClick={this.show}>Open</a>
+						</li>
+					</ul>
+					<h2>Chatbot</h2>
+					<div
+						ref={(el) => {
+							this.messagesEnd = el
+						}}
+						style={{ float: 'left', clear: 'both' }}
+					></div>
 				</div>
-			</>
-		)
+			)
+		}
 	}
 }
